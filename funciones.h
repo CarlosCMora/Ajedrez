f36df49ad5c8b8d59864d6a254ac2491999763e4
CarlosCMora <54331564+CarlosCMora@users.noreply.github.com>
2019-11-04 16:48:23 -0600

    Add files via upload

---
+++ funciones.h
@@ -0,0 +1,19 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include<stdio.h>
+#include "coordenadas.h"
+
+coord leer();
+int menu();
+void  caballo(coord a, coord b);
+void  rey(coord a, coord b);
+void  reina(coord a, coord b);
+void  torre(coord a, coord b);
+void  alfil(coord a, coord b);
+
+
+
+
+
+#endif
